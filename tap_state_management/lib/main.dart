572d5aee548caeced39ba29088486ebfe8c10b87
floreaadrian <florea.adrian.paul@gmail.com>
2019-07-03 18:51:44 +0300

    managing state: 3.A mix-and-match approach

--- tap_state_management/lib/main.dart
+++ tap_state_management/lib/main.dart
@@ -16,43 +16,6 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class TapboxA extends StatefulWidget {
-  TapboxA({Key key}) : super(key: key);
-
-  @override
-  _TapboxAState createState() => _TapboxAState();
-}
-
-class _TapboxAState extends State<TapboxA> {
-  bool _active = false;
-
-  void _handleTap() {
-    setState(() {
-      this._active = !this._active;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: this._handleTap,
-      child: Container(
-        child: Center(
-          child: Text(
-            _active ? 'Active' : 'Inactive',
-            style: TextStyle(fontSize: 32.0, color: Colors.white),
-          ),
-        ),
-        width: 200.0,
-        height: 200.0,
-        decoration: BoxDecoration(
-          color: _active ? Colors.lightGreen[700] : Colors.grey[600],
-        ),
-      ),
-    );
-  }
-}
-
 class ParentWidget extends StatefulWidget {
   @override
   _ParentWidgetState createState() => _ParentWidgetState();
@@ -70,37 +33,68 @@ class _ParentWidgetState extends State<ParentWidget> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: TapboxB(active: _active, onChanged: _handleTapboxChanged),
+      child: TapboxC(active: _active, onChanged: _handleTapboxChanged),
     );
   }
 }
 
-class TapboxB extends StatelessWidget {
-  TapboxB({Key key, this.active: false, @required this.onChanged})
+class TapboxC extends StatefulWidget {
+  TapboxC({Key key, this.active: false, @required this.onChanged})
       : super(key: key);
 
   final bool active;
   final ValueChanged<bool> onChanged;
 
+  _TapboxCState createState() => _TapboxCState();
+}
+
+class _TapboxCState extends State<TapboxC> {
+  bool _highlight = false;
+
+  void _handleTapDown(TapDownDetails details) {
+    setState(() {
+      _highlight = true;
+    });
+  }
+
+  void _handleTapUp(TapUpDetails details) {
+    setState(() {
+      _highlight = false;
+    });
+  }
+
+  void _handleTapCancel() {
+    setState(() {
+      _highlight = false;
+    });
+  }
+
   void _handleTap() {
-    onChanged(!active);
+    widget.onChanged(!widget.active);
   }
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
+      onTapDown: _handleTapDown,
+      onTapUp: _handleTapUp,
       onTap: _handleTap,
+      onTapCancel: _handleTapCancel,
       child: Container(
         child: Center(
-          child: Text(
-            active ? 'Active' : 'Inactive',
-            style: TextStyle(fontSize: 32.0, color: Colors.white),
-          ),
+          child: Text(widget.active ? 'Active' : 'Inactive',
+              style: TextStyle(fontSize: 32.0, color: Colors.white)),
         ),
         width: 200.0,
         height: 200.0,
         decoration: BoxDecoration(
-          color: active ? Colors.lightBlue[700] : Colors.grey[600],
+          color: widget.active ? Colors.lightGreen[700] : Colors.grey[600],
+          border: _highlight
+              ? Border.all(
+                  color: Colors.teal[700],
+                  width: 10.0,
+                )
+              : null,
         ),
       ),
     );
