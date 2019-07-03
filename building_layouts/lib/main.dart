eb0defa4875b0cd37a06226b37108d8bf69372c1
floreaadrian <florea.adrian.paul@gmail.com>
2019-07-03 14:38:07 +0300

    Pavlova app

--- building_layouts/lib/main.dart
+++ building_layouts/lib/main.dart
@@ -19,79 +19,142 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  Widget buildHome(BuildContext context) {
+    final descTextStyle = TextStyle(
+      color: Colors.black,
+      fontWeight: FontWeight.w800,
+      fontFamily: 'Roboto',
+      letterSpacing: 0.5,
+      fontSize: 18,
+      height: 2,
+    );
+
+    var stars = Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Icon(Icons.star, color: Colors.green[500]),
+        Icon(Icons.star, color: Colors.green[500]),
+        Icon(Icons.star, color: Colors.green[500]),
+        Icon(Icons.star, color: Colors.black),
+        Icon(Icons.star, color: Colors.black),
+      ],
+    );
+
+    final ratings = Container(
+      padding: EdgeInsets.all(20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          stars,
+          Text(
+            '170 Reviews',
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.w800,
+              fontFamily: 'Roboto',
+              letterSpacing: 0.5,
+              fontSize: 20,
+            ),
+          ),
+        ],
+      ),
+    );
+
+    final titleText = Container(
+      padding: EdgeInsets.all(20),
+      child: Text(
+        'Strawberry Pavlova',
+        style: TextStyle(
+          fontWeight: FontWeight.w800,
+          letterSpacing: 0.5,
+          fontSize: 30,
+        ),
+      ),
+    );
+
+    final subTitle = Text(
+      'Pavlova is a meringue-based dessert named after the Russian ballerina '
+      'Anna Pavlova. Pavlova features a crisp crust and soft, light inside, '
+      'topped with fruit and whipped cream.',
+      textAlign: TextAlign.center,
+      style: TextStyle(
+        fontFamily: 'Georgia',
+        fontSize: 25,
+      ),
+    );
+
+    final iconList = DefaultTextStyle.merge(
+        style: descTextStyle,
+        child: Container(
+          padding: EdgeInsets.all(20),
+          child: Row(
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  Icon(Icons.kitchen, color: Colors.green[500]),
+                  Text('PREP:'),
+                  Text('25 min'),
+                ],
+              ),
+              Column(
+                children: <Widget>[
+                  Icon(Icons.timer, color: Colors.green[500]),
+                  Text('COOK:'),
+                  Text('1 hr'),
+                ],
+              ),
+              Column(
+                children: <Widget>[
+                  Icon(Icons.restaurant, color: Colors.green[500]),
+                  Text('FEEDS:'),
+                  Text('4-6 min'),
+                ],
+              )
+            ],
+          ),
+        ));
+
+    final leftColumn = Container(
+      padding: EdgeInsets.fromLTRB(20, 30, 20, 20),
+      child: Column(
+        children: <Widget>[
+          titleText,
+          subTitle,
+          ratings,
+          iconList,
+        ],
+      ),
+    );
+
+    final mainImage = Image.asset(
+      'images/pavlova.jpg',
+      fit: BoxFit.cover,
+    );
+
+    return Container(
+      margin: EdgeInsets.fromLTRB(0, 40, 0, 30),
+      height: 600,
+      child: Card(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              width: 440,
+              child: leftColumn,
+            ),
+            mainImage
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('hatz'),
-      ),
-      body: Container(
-          child: Row(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          Column(
-            children: <Widget>[
-              IconButton(
-                icon: Icon(
-                  Icons.call,
-                  color: Theme.of(context).primaryColor,
-                ),
-                onPressed: null,
-                tooltip: 'call',
-              ),
-              Container(
-                child: Text(
-                  'CALL',
-                  style: TextStyle(
-                    color: Theme.of(context).primaryColor,
-                  ),
-                ),
-              )
-            ],
-          ),
-          Column(
-            children: <Widget>[
-              IconButton(
-                icon: Icon(
-                  Icons.router,
-                  color: Theme.of(context).primaryColor,
-                ),
-                onPressed: null,
-                tooltip: 'route',
-              ),
-              Container(
-                child: Text(
-                  'ROUTE',
-                  style: TextStyle(
-                    color: Theme.of(context).primaryColor,
-                  ),
-                ),
-              )
-            ],
-          ),
-          Column(
-            children: <Widget>[
-              IconButton(
-                icon: Icon(
-                  Icons.share,
-                  color: Theme.of(context).primaryColor,
-                ),
-                onPressed: null,
-                tooltip: 'share',
-              ),
-              Container(
-                child: Text(
-                  'SHARE',
-                  style: TextStyle(
-                    color: Theme.of(context).primaryColor,
-                  ),
-                ),
-              )
-            ],
-          )
-        ],
-      )),
-    );
+        appBar: AppBar(
+          title: Text('hatz'),
+        ),
+        body: buildHome(context));
   }
 }
