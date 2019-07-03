c827b4c6dfab94bbb37d9f916acebe9309f809d0
floreaadrian <florea.adrian.paul@gmail.com>
2019-07-03 15:47:40 +0300

    starting building_layouts_tutorial_app

---
+++ building_layouts_tutorial_app/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter layout demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter layout demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  MyHomePage({this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(this.title),
+      ),
+    );
+  }
+}
