b632932224f05e7e0b3257debbc78a19ab6b7d5c
floreaadrian <florea.adrian.paul@gmail.com>
2019-07-04 17:07:09 +0300

    Simplifying with Animated­Widget

--- Animations/animation_tutorial/lib/main.dart
+++ Animations/animation_tutorial/lib/main.dart
@@ -15,15 +15,29 @@ class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
     super.initState();
     animationController =
         AnimationController(duration: const Duration(seconds: 2), vsync: this);
-    animation = Tween<double>(begin: 0, end: 300).animate(animationController)
-      ..addListener(() {
-        setState(() {});
-      });
+    animation = Tween<double>(begin: 0, end: 300).animate(animationController);
     animationController.forward();
   }
 
   @override
+  Widget build(BuildContext context) => AnimatedLogo(
+        animation: animation,
+      );
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+}
+
+class AnimatedLogo extends AnimatedWidget {
+  AnimatedLogo({Key key, Animation<double> animation})
+      : super(key: key, listenable: animation);
+
+  @override
   Widget build(BuildContext context) {
+    final Animation<double> animation = listenable;
     return Center(
       child: Container(
         margin: EdgeInsets.symmetric(vertical: 10),
@@ -32,11 +46,5 @@ class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
         child: FlutterLogo(),
       ),
     );
-  }
-
-  @override
-  void dispose() {
-    animationController.dispose();
-    super.dispose();
   }
 }
