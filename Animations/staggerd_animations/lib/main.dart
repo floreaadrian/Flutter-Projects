6c40ffea535f49d3cbaf48673e703dcd5c68e0f3
floreaadrian <florea.adrian.paul@gmail.com>
2019-07-05 15:47:56 +0300

    Staggered Animations

---
+++ Animations/staggerd_animations/lib/main.dart
@@ -0,0 +1,183 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: StaggerDemo(),
+    );
+  }
+}
+
+class StaggerAnimation extends StatelessWidget {
+  StaggerAnimation({Key key, this.controller})
+      : opacity = Tween<double>(
+          begin: 0.0,
+          end: 1.0,
+        ).animate(
+          CurvedAnimation(
+            parent: controller,
+            curve: Interval(
+              0.0,
+              0.100,
+              curve: Curves.ease,
+            ),
+          ),
+        ),
+        width = Tween<double>(
+          begin: 50.0,
+          end: 150.0,
+        ).animate(
+          CurvedAnimation(
+            parent: controller,
+            curve: Interval(
+              0.125,
+              0.250,
+              curve: Curves.ease,
+            ),
+          ),
+        ),
+        height = Tween<double>(begin: 50.0, end: 150.0).animate(
+          CurvedAnimation(
+            parent: controller,
+            curve: Interval(
+              0.250,
+              0.375,
+              curve: Curves.ease,
+            ),
+          ),
+        ),
+        padding = EdgeInsetsTween(
+          begin: const EdgeInsets.only(bottom: 16.0),
+          end: const EdgeInsets.only(bottom: 75.0),
+        ).animate(
+          CurvedAnimation(
+            parent: controller,
+            curve: Interval(
+              0.250,
+              0.375,
+              curve: Curves.ease,
+            ),
+          ),
+        ),
+        borderRadius = BorderRadiusTween(
+          begin: BorderRadius.circular(4.0),
+          end: BorderRadius.circular(75.0),
+        ).animate(
+          CurvedAnimation(
+            parent: controller,
+            curve: Interval(
+              0.375,
+              0.500,
+              curve: Curves.ease,
+            ),
+          ),
+        ),
+        color = ColorTween(
+          begin: Colors.indigo[100],
+          end: Colors.orange[400],
+        ).animate(
+          CurvedAnimation(
+            parent: controller,
+            curve: Interval(
+              0.500,
+              0.750,
+              curve: Curves.ease,
+            ),
+          ),
+        ),
+        super(key: key);
+
+  final Animation<double> controller;
+  final Animation<double> opacity;
+  final Animation<double> width;
+  final Animation<double> height;
+  final Animation<EdgeInsets> padding;
+  final Animation<BorderRadius> borderRadius;
+  final Animation<Color> color;
+
+  Widget _buildWidget(BuildContext context, Widget child) {
+    return Container(
+      padding: padding.value,
+      alignment: Alignment.bottomCenter,
+      child: Opacity(
+        opacity: opacity.value,
+        child: Container(
+          width: width.value,
+          height: height.value,
+          decoration: BoxDecoration(
+              color: color.value,
+              border: Border.all(color: Colors.indigo[300], width: 3.0),
+              borderRadius: borderRadius.value),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      builder: _buildWidget,
+      animation: controller,
+    );
+  }
+}
+
+class StaggerDemo extends StatefulWidget {
+  StaggerDemo({Key key}) : super(key: key);
+
+  _StaggerDemoState createState() => _StaggerDemoState();
+}
+
+class _StaggerDemoState extends State<StaggerDemo>
+    with TickerProviderStateMixin {
+  AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller =
+        AnimationController(vsync: this, duration: const Duration(seconds: 2));
+  }
+
+  Future<void> _playAnimation() async {
+    try {
+      await _controller.forward().orCancel;
+      await _controller.reverse().orCancel;
+    } on TickerCanceled {}
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: CupertinoNavigationBar(
+          middle: Text('ceao'),
+        ),
+        body: GestureDetector(
+          behavior: HitTestBehavior.opaque,
+          onTap: () => _playAnimation(),
+          child: Center(
+            child: Container(
+              width: 300.0,
+              height: 300.0,
+              decoration: BoxDecoration(
+                color: Colors.purple.withOpacity(0.1),
+                border: Border.all(
+                  color: Colors.black.withOpacity(0.5),
+                ),
+              ),
+              child: StaggerAnimation(
+                controller: _controller,
+              ),
+            ),
+          ),
+        ));
+  }
+}
