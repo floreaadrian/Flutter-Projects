92249e59420f5e4bf30fab605f181db266bf8325
floreaadrian <florea.adrian.paul@gmail.com>
2019-09-09 14:42:17 +0300

    refecatoring the tests

--- tdd_todo_app/test/features/todo_app/domain/repositories/task_repository_test.dart
+++ tdd_todo_app/test/features/todo_app/domain/repositories/task_repository_test.dart
@@ -5,6 +5,34 @@ import 'package:tdd_todo_app/features/todo_app/domain/entities/task.dart';
 import 'package:tdd_todo_app/features/todo_app/domain/repositories/task_repository.dart';
 
 void main() {
+  addTest();
+  deleteTest();
+}
+
+Future deleteTest() async {
+  TaskRepositoryContract taskRepository = new TaskRepository();
+  test(
+    'should delete a task',
+    () async {
+      // arrange
+      taskRepository = new TaskRepository();
+      Task task = Task(
+        id: 0,
+        added: new DateTime(2017, 9, 9, 9),
+        isDone: false,
+        text: "ceva",
+      );
+      // act
+      final deleteBeforeAdding = await taskRepository.delete(task);
+      expect(deleteBeforeAdding, false);
+      await taskRepository.add(task);
+      final deleteAfterAdding = await taskRepository.delete(task);
+      expect(deleteAfterAdding, true);
+    },
+  );
+}
+
+void addTest() async {
   TaskRepositoryContract taskRepository = new TaskRepository();
   test(
     'should add a task',
@@ -28,26 +56,6 @@ void main() {
       // assert
       expect(added, true);
       expect(addedSecondTime, false);
-    },
-  );
-
-  test(
-    'should delete a task',
-    () async {
-      // arrange
-      taskRepository = new TaskRepository();
-      Task task = Task(
-        id: 0,
-        added: new DateTime(2017, 9, 9, 9),
-        isDone: false,
-        text: "ceva",
-      );
-      // act
-      final deleteBeforeAdding = await taskRepository.delete(task);
-      expect(deleteBeforeAdding, false);
-      await taskRepository.add(task);
-      final deleteAfterAdding = await taskRepository.delete(task);
-      expect(deleteAfterAdding, true);
     },
   );
 }
